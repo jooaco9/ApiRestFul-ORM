96556bbd74a973c4b446f391c93f273273fc2a3e
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-22 19:36:07 -0300

    se agregaron dos funciones, una para borrar una categoria y otra para obtener una categoria por id

--- api/controllers/category_controller.py
+++ api/controllers/category_controller.py
@@ -1,6 +1,7 @@
 from sqlmodel import select
 from api import CategoryModel, CategorySchema
 
+# Devuelve todas las categorias
 async def get_categories(db):
     # Crear la consulta
     stmt = select(CategoryModel)
@@ -11,6 +12,14 @@ async def get_categories(db):
 
     return categories
 
+# Devuelve una categoria
+async def get_category(category_id, db):
+    # Obtenemos la categoria
+    category = db.get(CategoryModel, category_id)
+
+    return category
+
+# Agrega una categoria
 async def write_category(category: CategorySchema, db):
     # Creo el modelo ORM a partir del schema
     category_model = CategoryModel(name=category.name)
@@ -27,6 +36,7 @@ async def write_category(category: CategorySchema, db):
 
     return category_model
 
+# Actualiza una categoria
 async def update_category(category_id, category: CategorySchema, db):
     # Verificar que haya categoria con el id de category
     existing_category = db.get(CategoryModel, category_id)
@@ -43,3 +53,31 @@ async def update_category(category_id, category: CategorySchema, db):
     db.refresh(existing_category)
 
     return existing_category
+
+
+# Borra una categoria
+async def delete_category(category_id, db):
+    category = db.get(CategoryModel, category_id)
+
+    # Verifico que exista categoria con el id
+    if not category:
+        return None
+
+    # Borro categoria y commiteo
+    db.delete(category)
+    db.commit()
+
+    return category
+
+
+
+
+
+
+
+
+
+
+
+
+

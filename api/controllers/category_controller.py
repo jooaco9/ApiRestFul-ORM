2678b5a670356bc4e2d6d34a0ab8c61779c5ccdc
jooaco9 <joaquin.corbo9@gmail.com>
2025-05-21 22:31:56 -0300

    dos funciones nuevas, una para actualizar las categorias y otra para agregar

--- api/controllers/category_controller.py
+++ api/controllers/category_controller.py
@@ -1,7 +1,7 @@
 from sqlmodel import select
-from api import CategoryModel
+from api import CategoryModel, CategorySchema
 
-def get_categories(db):
+async def get_categories(db):
     # Crear la consulta
     stmt = select(CategoryModel)
 
@@ -10,3 +10,36 @@ def get_categories(db):
     categories = result.all()
 
     return categories
+
+async def write_category(category: CategorySchema, db):
+    # Creo el modelo ORM a partir del schema
+    category_model = CategoryModel(name=category.name)
+    print(category_model)
+
+    # Agregamos la categoria a la sesion
+    db.add(category_model)
+
+    # Confirmo el cambio
+    db.commit()
+
+    # Se refresca para obtener el id generado
+    db.refresh(category_model)
+
+    return category_model
+
+async def update_category(category_id, category: CategorySchema, db):
+    # Verificar que haya categoria con el id de category
+    existing_category = db.get(CategoryModel, category_id)
+    if not existing_category:
+        return None
+
+    # Actualizar la category
+    existing_category.name = category.name
+
+    # Confirmo los cambios
+    db.commit()
+
+    # Refrescar para obtener los datos actualizados
+    db.refresh(existing_category)
+
+    return existing_category
